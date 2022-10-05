142fa21ae80a79607cab187fce8a82bc079cc5ca
Marita Kozak <kozakmarita@gmail.com>
2022-10-05 10:26:39 +0300

    Homework2 added

---
+++ Homework2/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число 
+// и удаляет вторую цифру этого числа. Не использовать строки для расчета.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Дано число: {number}");
+int newNumber = 0;
+
+int FoundNumber()
+{
+    newNumber = number / 100 * 10 + number % 10;
+    return newNumber;
+}
+
+FoundNumber();
+Console.WriteLine($"Если из числа {number} удалить вторую цифру, получится число {newNumber}");
+
+
---
+++ Homework2/Task3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 3: Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет. Не использовать строки для расчета.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 326792 -> 6
+
+int num, count, foundNumber;
+
+Console.WriteLine("Введите число: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+int FoundNumber(int number)
+{
+    count = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        count++;
+    }
+    return count;
+}
+
+foundNumber = FoundNumber(num);
+
+if (foundNumber <= 2)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    if (foundNumber > 3)
+    {
+        num = num / Convert.ToInt32(Math.Pow(10, foundNumber - 3)); 
+    }
+    num = num % 10;
+    Console.WriteLine($"Третья цифра введённого числа: {num}");
+}
+
+
+
+
