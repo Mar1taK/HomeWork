f741e37ec8fe327a394b43c19b4884f578657d46
Marita Kozak <kozakmarita@gmail.com>
2022-10-15 20:25:17 +0300

    Homework5 added

---
+++ Homework5/Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Задайте размер массива:  ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[sizeArray];
+int count = 0;
+
+void GetArrayRandomNumbers(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100, 1000);
+    }
+    Console.WriteLine("Массив сгенерирован: ");
+}
+
+void GetEvenNumber(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+        if (numbers[i] % 2 == 0)
+            count++;
+    Console.WriteLine($"В массиве {numbers.Length} чисел, {count} из них чётные");
+}
+
+void PrintArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
+GetArrayRandomNumbers(numbers);
+PrintArray(numbers);
+GetEvenNumber(numbers);
---
+++ Homework5/Task3/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+Console.WriteLine("Введите размер массива  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+int min = Int32.MaxValue;
+int max = Int32.MinValue;
+
+void GetArrayRandomNumbers(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = Convert.ToInt32(new Random().Next(1, 1000));
+    }
+    Console.WriteLine("Массив сгенерирован: ");
+}
+void PrintArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
+void GetDifNumber(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > max)
+        {
+            max = numbers[i];
+        }
+        if (numbers[i] < min)
+        {
+            min = numbers[i];
+        }
+    }
+}
+GetArrayRandomNumbers(numbers);
+PrintArray(numbers);
+GetDifNumber(numbers);
+Console.WriteLine($"Всего {numbers.Length} чисел. Максимальное значение -> {max}, минимальное значение -> {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением -> {max - min}");
+
+
