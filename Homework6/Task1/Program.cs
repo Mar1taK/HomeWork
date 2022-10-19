2d7ddda92bfd8b05c3b2986b74451981a9c87030
Marita Kozak <kozakmarita@gmail.com>
2022-10-19 22:49:06 +0300

    Honework6 added

---
+++ Homework6/Task1/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+// Разделить ввод данных, от расчета данных (Одна функция вводит данные, другая функция подсчитывает количество положительных)
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите числа через запятую: ");
+int[] numbers = GetDataEntry(Console.ReadLine());
+
+int[] GetDataEntry(string input)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ',')
+        {
+            count++;
+        }
+    }
+
+    int[] numbers = new int[count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+
+        while (input[i] != ',')
+        {
+            if (i != input.Length - 1)
+            {
+                temp += input[i].ToString();
+                i++;
+            }
+            else
+            {
+                temp += input[i].ToString();
+                break;
+            }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
+}
+
+void GetPositiveNumber(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 0)
+        {
+            sum++;
+        }
+    }
+    Console.WriteLine($"Количество значений больше 0 -> {sum}");
+}
+GetPositiveNumber(numbers);
+
+
+
+
+
+
