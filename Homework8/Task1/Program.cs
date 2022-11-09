ff83fa9dc0ddd920af6f60003083b27de5fc3182
Marita Kozak <kozakmarita@gmail.com>
2022-11-09 22:01:49 +0300

    Homework8 added

---
+++ Homework8/Task1/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 1: Задайте двумерный массив. Напишите программу, которая
+// упорядочивает по убыванию элементы каждой строки двумерного массива.
+
+Console.WriteLine("Введите количество строк: ");
+int lines = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[lines, columns];
+GetArrayRandomNumbers(numbers);
+Console.WriteLine();
+Console.WriteLine("Массив до сортировки: ");
+PrintArray(numbers);
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1) - 1; j++)
+    {
+        for (int z = 0; z < numbers.GetLength(1) - 1; z++)
+        {
+            if (numbers[i, z] < numbers[i, z + 1]) 
+            {
+                int temp = 0;
+                temp = numbers[i, z];
+                numbers[i, z] = numbers[i, z + 1];
+                numbers[i, z + 1] = temp;
+            }
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Отсортированный по убыванию массив: ");
+PrintArray(numbers);
+
+void GetArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
