150ab6e07535bec1b30a5146e45fd27b9533e5c6
Marita Kozak <kozakmarita@gmail.com>
2022-11-13 13:52:54 +0300

    Homework9 added

---
+++ Homework9/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные 
+// натуральные числа в промежутке от M до N с помощью рекурсии.
+
+Console.Write("Введите значение M: ");
+int M = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int N = Convert.ToInt16(Console.ReadLine());
+
+void GetEvenNumber(int M, int N)
+{
+    if (N < M) return;
+    if (N % 2 == 0) GetEvenNumber(M, N - 2);
+    else GetEvenNumber(M, N - 1);
+    if (N % 2 == 0) Console.Write(N + " ");
+}
+
+GetEvenNumber(M, N);
+Console.WriteLine();
+
+
---
+++ Homework9/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+Console.Write("Введите M: ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма элементов в промежутке от M до N => {GetSumNumbers(M, N)}");
+Console.ReadLine();
+
+int GetSumNumbers(int M, int N)
+{
+    if (M == 0) return (N * (N + 1)) / 2;            
+    else if (N == 0) return (M * (M + 1)) / 2;       
+    else if (M == N) return M;                       
+    else if (M < N) return N + GetSumNumbers(M, N - 1); 
+    else return N + GetSumNumbers(M, N + 1);            
+}
+
